718fc34d200a9b86ae17ad830cd99b943c97a30e
alexsumoski <alexsumoski@gmail.com>
2022-08-19 23:42:55 -0400

    updates

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const fs = require("fs");
 
 const path = require("path");
 const pathToFile = path.resolve("./data.json");
-const getResources = () => JSON.parse(fs.readFileSync(pathToFile))
+const cors = require("cors");
+
+var corsOptions = {
+  origin: 'https://localhost:3001/api/resources',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+
+const getResources = () => JSON.parse(fs.readFileSync(pathToFile));
 
 
 app.get("/", (req, res) => {
